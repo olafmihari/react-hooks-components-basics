5a9842e1ec0cb5de2604364824921b784952db52
Olaf Mihari <olafnjenga@gmail.com>
2023-04-18 06:37:41 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
-import Article from "./Article";
-import Comment from "./Comment";
+
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <Article />
-      <Comment />
+      <Navbar />
+      <Home />
+      <About/>
     </div>
   );
 }
